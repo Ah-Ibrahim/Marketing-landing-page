7971671ff66d112c689a6cb6de87eefd853c7654
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-08-14 17:38:10 +0300

    Refactored PageDetails.tsx

--- src/components/PageDetails.tsx
+++ src/components/PageDetails.tsx
@@ -1,9 +1,13 @@
-import HeroSection from './HeroSection';
+import type { ReactNode } from 'react';
 
-function PageDetails() {
+type PageDetailsProps = {
+    children: ReactNode;
+};
+
+function PageDetails({ children }: PageDetailsProps) {
     return (
-        <main className="bg-bg-primary mt-4 grid items-center rounded-md py-12">
-            <HeroSection />
+        <main className="bg-bg-primary mt-4 grid justify-items-center rounded-md py-12">
+            {children}
         </main>
     );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,14 @@
 import NavBar from '../components/NavBar';
 import PageDetails from '../components/PageDetails';
+import HeroSection from '../components/HeroSection';
 
 function LandingPage() {
     return (
         <div className="bg-bg-secondary page p-4">
             <NavBar />
-            <PageDetails />
+            <PageDetails>
+                <HeroSection />
+            </PageDetails>
         </div>
     );
 }

aa9e16a78d6be66364eb804712cfe40cac875c08
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-08-31 17:22:14 +0300

    Added: Hero Image and font preloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { preload } from 'react-dom';
+import fontSrc from './assets/fonts/Noto_Sans/NotoSans-VariableFont_wdth,wght.ttf';
+import heroImageSrc from './assets/images/prism.png';
 import LandingPage from './pages/LandingPage';
 
 function App() {
+    preload(heroImageSrc, { as: 'image', fetchPriority: 'high' });
+    preload(fontSrc, {
+        as: 'font',
+    });
+
     return (
         <>
             <LandingPage />
